9f7730abf3d4e96cc2a0bf98369dd06a5faf290f
ItzNxhin <nahinpenarandamejia@hotmail.com>
2024-02-26 14:57:59 -0500

    Cambios

--- src/main/java/controller/Fachada.java
+++ src/main/java/controller/Fachada.java
@@ -15,7 +15,7 @@ public class Fachada implements ActionListener{
 
     public Fachada(){
         this.vista = new Ventana();
-        
+        //prueba
         this.vista.jrAplazado.addActionListener(this);
         this.vista.jrMenorEdad.addActionListener(this);
         this.vista.jrRecluta.addActionListener(this);
